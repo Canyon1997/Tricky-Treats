c11776bb719fbd98a534844c9283cfff8cc5327c
Canyon1997 <canyon.davidson@gmail.com>
2020-10-31 11:00:38 -0400

    Player Controller Setups

---
+++ Assets/Scripts/Player/Doggo/DoggoController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoggoController : UniversalPlayerItems
+{
+    private Rigidbody2D doggo_RB;
+
+    private Vector2 movement;
+
+    [Header("Doggo Move Speed!")]
+    public float doggo_MoveSpeed;
+
+    //Setting Inherited Fields
+    public override Rigidbody2D Rigidbody
+    {
+        get
+        {
+            return doggo_RB;
+        }
+    }
+
+    public override float Speed
+    {
+        get
+        {
+            return doggo_MoveSpeed;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doggo_RB = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        //Input for movement
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+    void FixedUpdate()
+    {
+        //Rigidbody movement
+        Rigidbody.MovePosition((Vector2) transform.position + movement * doggo_MoveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/UniversalPlayerItems.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UniversalPlayerItems : MonoBehaviour
+{
+  public abstract Rigidbody2D Rigidbody { get; }
+
+  public abstract float Speed { get; }
+}
