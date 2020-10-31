7ca44093d7443a014e138a84fd211f21cecf4135
Canyon1997 <canyon.davidson@gmail.com>
2020-10-31 11:15:09 -0400

    Basket Movement

---
+++ Assets/Scripts/Player/Candy Baskin/BasketController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController : UniversalPlayerItems
+{
+    private Rigidbody2D basket_RB;
+    private Vector2 movement;
+
+    [Header("Basket Speed")]
+    public float basket_MoveSpeed;
+
+    //Setting Inherited Fields
+    public override Rigidbody2D Rigidbody
+    {
+        get
+        {
+            return basket_RB;
+        }
+    }
+
+    public override float Speed
+    {
+        get
+        {
+            return basket_MoveSpeed;
+        }
+    }
+
+
+
+
+
+    void Start()
+    {
+        basket_RB = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        basket_RB.MovePosition((Vector2)transform.position + movement * basket_MoveSpeed * Time.fixedDeltaTime);
+    }
+}
