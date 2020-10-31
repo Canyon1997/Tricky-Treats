e0c8df15d00f54a9b5c1bc8741770df543a56a04
Canyon1997 <canyon.davidson@gmail.com>
2020-10-31 10:28:04 -0400

    Universal Items & Movement Scripts
    Setup of Items Scripts and Player Controller Movement added

---
+++ Assets/Scripts/Pickup Items/Treats/BakedCandy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BakedCandy : UniversalItemData
+{
+    private Rigidbody2D bakedCandy_RB;
+
+    [Header("Baked Candy Point Amount")]
+    [SerializeField] private int bakedCandy_Points = 40;
+
+    //Setting Inherited Fields
+    public override Rigidbody2D rigidbody2d
+    {
+        get
+        {
+            return bakedCandy_RB;
+        }
+    }
+
+    public override int points
+    {
+        get
+        {
+            return bakedCandy_Points;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Pickup Items/Treats/KingCandy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingCandy : UniversalItemData
+{
+    private Rigidbody2D kingCandy_RB;
+
+    [Header("King Candy Point Amount")]
+    [SerializeField] private int kingCandy_Points = 20;
+
+
+    //Setting Inherited Fields
+    public override Rigidbody2D rigidbody2d
+    {
+        get
+        {
+            return kingCandy_RB;
+        }
+    }
+
+    public override int points
+    {
+        get
+        {
+            return kingCandy_Points;
+        }
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
