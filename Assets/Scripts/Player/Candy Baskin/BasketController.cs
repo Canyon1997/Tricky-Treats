91bbcb1b68fb677ecb739b61dded4acd0d88822d
Canyon1997 <canyon.davidson@gmail.com>
2020-10-31 22:39:24 -0400

    Item Minigame Functionality

--- Assets/Scripts/Player/Candy Baskin/BasketController.cs
+++ Assets/Scripts/Player/Candy Baskin/BasketController.cs
@@ -11,8 +11,9 @@ public class BasketController : UniversalPlayerItems
     [SerializeField] private GameObject gameController;
     private GameController controller;
 
-    [Header("Treats")]
-    [SerializeField] private GameObject kingCandy;
+    [Header("Trick or Treats")]
+    [SerializeField] private GameObject[] itemCollectables;
+    private ItemScript itemID;
 
     [Header("Basket Speed")]
     public float basket_MoveSpeed;
@@ -44,10 +45,6 @@ public class BasketController : UniversalPlayerItems
 
         //GameController
         controller = gameController.GetComponent<GameController>();
-
-        //Treats
-
-        //Tricks
     }
 
 
@@ -55,11 +52,55 @@ public class BasketController : UniversalPlayerItems
     {
         movement.x = Input.GetAxisRaw("Horizontal");
 
-        controller.score++;
     }
 
     private void FixedUpdate()
     {
         basket_RB.MovePosition((Vector2)transform.position + movement * basket_MoveSpeed * Time.fixedDeltaTime);
+    }
+
+    //Adds or removes points based on the ItemID
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.GetComponent<ItemScript>().item == ItemScript.ItemID.LittleCandy)
+        {
+            controller.score += 10;
+            Destroy(other.gameObject);
+        }
+        else if(other.gameObject.GetComponent<ItemScript>().item == ItemScript.ItemID.KingCandy)
+        {
+            controller.score += 20;
+            Destroy(other.gameObject);
+        }
+        else if(other.gameObject.GetComponent<ItemScript>().item == ItemScript.ItemID.BakedCandy)
+        {
+            controller.score += 40;
+            Destroy(other.gameObject);
+        }
+        else if(other.gameObject.GetComponent<ItemScript>().item == ItemScript.ItemID.ToothBrush)
+        {
+            controller.score -= 10;
+            Destroy(other.gameObject);
+        }
+        else if(other.gameObject.GetComponent<ItemScript>().item == ItemScript.ItemID.Rock)
+        {
+            controller.score -= 20;
+            Destroy(other.gameObject);
+        }
+        else if(other.gameObject.GetComponent<ItemScript>().item == ItemScript.ItemID.Pencil)
+        {
+            controller.score -= 10;
+            Destroy(other.gameObject);
+        }
+        else if(other.gameObject.GetComponent<ItemScript>().item == ItemScript.ItemID.VampireTeeth)
+        {
+            controller.score -= 20;
+            Destroy(other.gameObject);
+        }
+        else if(other.gameObject.GetComponent<ItemScript>().item == ItemScript.ItemID.SpiderRing)
+        {
+            controller.score -= 10;
+            Destroy(other.gameObject);
+        }
     }
 }
