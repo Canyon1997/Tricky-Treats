6f3d39440609553f1afa073e7db69fe15adab6c7
Canyon1997 <canyon.davidson@gmail.com>
2020-10-31 22:01:11 -0400

    Item Identification Added

---
+++ Assets/Scripts/Pickup Items/ItemScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody2D rb;
+
+    public ItemID item;
+
+    public enum ItemID
+    {
+        LittleCandy,
+        KingCandy,
+        BakedCandy,
+        ToothBrush,
+        Rock,
+        Pencil,
+        VampireTeeth,
+        SpiderRing
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        switch(this.gameObject.tag)
+        {
+            case "LittleCandy":
+                item = ItemID.LittleCandy;
+                break;
+
+            case "KingCandy":
+                item = ItemID.KingCandy;
+                break;
+
+            case "BakedCandy":
+                item = ItemID.BakedCandy;
+                break;
+
+            case "ToothBrush":
+                item = ItemID.ToothBrush;
+                break;
+
+            case "Rock":
+                item = ItemID.Rock;
+                break;
+
+            case "Pencil":
+                item = ItemID.Pencil;
+                break;
+
+            case "VampireTeeth":
+                item = ItemID.VampireTeeth;
+                break;
+
+            case "SpiderRing":
+                item = ItemID.SpiderRing;
+                break;
+
+        }
+    }
+}
